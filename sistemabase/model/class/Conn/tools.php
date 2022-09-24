028a015814e0f3f07906a65dc181d673df2df231
Javier <javierlin2019@gmail.com>
2022-09-24 17:36:06 -0600

    Fin de clase 24/09

--- sistemabase/model/class/Conn/tools.php
+++ sistemabase/model/class/Conn/tools.php
@@ -1,7 +1,27 @@
 <?php
 
 class Tools{
-    function conectar()
+    
+    function conectar(){
+    $conexion=mysql_connect(SERVER,USERDB,PASSDB,DATABASE);
+
+    if($conexion){
+        mysql_query($conexion, "SET NAMES 'utf8'");
+        mysqli_set_charset($conexion,"utf8");
+    }else{
+        echo "Error de conexion debido a: <br> ".mysqli_conect_error();
+    }
+    return $conexion;
+    }
+
+    function desconectar($conexion){
+        $close = mysqli_close($conexion);
+        
+        if(!$close){
+            echo "Ocurrio un error al cerrar la conexion debido a: <br> ".mysqli_connect_error();
+        }
+        return $close;
+    }
 
 }
 ?>
